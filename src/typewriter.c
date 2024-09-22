c6ebc3b558e9cc42f8be17805b47feb3e337280b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-22 18:37:50 +0530

    Setup CMake

--- src/typewriter.c
+++ src/typewriter.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 // ------------------------
 // TYPEWRITER CONFIGURATION
@@ -21,12 +26,17 @@ typedef struct
 /// @param text The text to write to the screen
 void typewriter(const TypeWriterConfig *cfg)
 {
-    int len = strlen(cfg->text);
+    size_t len = strlen(cfg->text);
     int pauseFor = (60 * 1000) / cfg->cpm;
 
     for (int i = 0; i < len; i++)
     {
         printf("%c", cfg->text[i]);
+
+#ifdef _WIN32
+        Sleep(pauseFor * 1000);
+#else
         usleep(pauseFor * 1000);
+#endif
     }
 }
