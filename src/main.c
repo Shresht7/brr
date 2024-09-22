45cdcbe5ff2e9b5f941367502fb2b67ed0c2a0f8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-22 16:39:41 +0530

    Handle dynamic allocation of memory when reading `STDIN`

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
@@ -15,6 +16,46 @@ void typewriter(const char *text)
     }
 }
 
+/// @brief Read the contents from `STDIN`
+/// @return A string
+char *read_stdin()
+{
+    size_t size = 1024; // Initial size of the memory allocation for the buffer
+    size_t len = 0;     // The current size of the buffer
+
+    // Instantiate a buffer and dynamically allocate memory for it
+    char *buffer = malloc(size);
+    if (!buffer)
+    {
+        fprintf(stderr, "Memory Allocation Error\n");
+        exit(1);
+    }
+
+    // Iterate over the characters from stdin
+    int c;
+    while ((c = fgetc(stdin)) != EOF)
+    {
+        // If we are going to go over the allocated size ...
+        if (len + 1 >= size)
+        {
+            // Resize the buffer and reallocate the memory
+            size *= 2;
+            buffer = realloc(buffer, size);
+            if (!buffer)
+            {
+                fprintf(stderr, "Memory Allocation Error\n");
+                exit(1);
+            }
+        }
+
+        // Store the character in the buffer
+        buffer[len++] = (char)c;
+    }
+
+    buffer[len] = '\0'; // Close out the buffer with a NULL character
+    return buffer;
+}
+
 // ====
 // MAIN
 // ====
@@ -23,22 +64,14 @@ void typewriter(const char *text)
 int main()
 {
 
-    char buffer[1024]; // Buffer to store the data coming in from stdin
-    char *text;        // The text to write to the console
-
-    // Read the contents from the stdin
-    if (fgets(buffer, sizeof(buffer), stdin) != NULL)
-    {
-        text = buffer;
-    }
-    else
-    {
-        fprintf(stderr, "Error reading input\n");
-        return 1; // Exit code 1 for failure
-    }
+    // Read the text from STDIN
+    char *text = read_stdin();
 
     // Write it to the console like a typewriter
     typewriter(text);
+
+    // Free the allocated memory
+    free(text);
 
     return 0; // Exit code 0 for success
 }
