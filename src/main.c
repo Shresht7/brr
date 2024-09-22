bac8412aad55a19f26aa73b3a7d383031171760e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-22 14:56:55 +0530

    Read contents from `STDIN`

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <time.h>
 
+/// @brief Writes the given text like a typewriter
+/// @param text The text to write to the screen
 void typewriter(const char *text)
 {
     int pauseFor = 100;
@@ -13,9 +15,30 @@ void typewriter(const char *text)
     }
 }
 
+// ====
+// MAIN
+// ====
+
+// The main entry point of the application
 int main()
 {
-    char *text = "Hello World!";
+
+    char buffer[1024]; // Buffer to store the data coming in from stdin
+    char *text;        // The text to write to the console
+
+    // Read the contents from the stdin
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        text = buffer;
+    }
+    else
+    {
+        fprintf(stderr, "Error reading input\n");
+        return 1; // Exit code 1 for failure
+    }
+
+    // Write it to the console like a typewriter
     typewriter(text);
-    return 0;
+
+    return 0; // Exit code 0 for success
 }
