f1416bca3f429cd6b7c231208fff024d659675ca
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-22 14:27:45 +0530

    Create a basic implementation of the typewriter

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+void typewriter(const char *text)
+{
+    int pauseFor = 100;
+    while (*text)
+    {
+        printf("%c", *text);
+        text++;
+        usleep(pauseFor * 1000);
+    }
+}
 
 int main()
 {
-    printf("Hello World!");
+    char *text = "Hello World!";
+    typewriter(text);
     return 0;
 }
